a0029c3dccb5c6a98bb4f4287e0ba209ec81b02e
MisKuchum <MisKuchum@gmail.com>
2020-06-04 15:02:21 +0300

    fixed

--- src/main/java/ru/mikhail/katya/lab1/factories/PdfTestFactory.java
+++ src/main/java/ru/mikhail/katya/lab1/factories/PdfTestFactory.java
@@ -1,14 +1,16 @@
 package ru.mikhail.katya.lab1.factories;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.mikhail.katya.lab1.pojo.DocumentProxy;
 import ru.mikhail.katya.lab1.pojo.Pdf;
 
 @Component
 public class PdfTestFactory implements DocumentFactory {
-
+    @Autowired
+    private Pdf pdf;
     public DocumentProxy getDocument() {
-        return DocumentProxy.of(new Pdf("pdf-name", "pdf-content"));
+        return DocumentProxy.of(pdf);
     }
 
 }
--- src/main/java/ru/mikhail/katya/lab1/pojo/Pdf.java
+++ src/main/java/ru/mikhail/katya/lab1/pojo/Pdf.java
@@ -1,16 +1,19 @@
 package ru.mikhail.katya.lab1.pojo;
 
+import jdk.jfr.ContentType;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.springframework.stereotype.Component;
 
+@Component
 @EqualsAndHashCode(callSuper = false)
 @Data
 @ToString(callSuper = true)
 public class Pdf extends Document {
 
-    public Pdf(String name, String content) {
-        super(name, content);
-    }
-
+//    public Pdf(String name, String content) {
+//        super(name, content);
+//    }
+    public Pdf(){ super("pdf-name", "pdf-content"); }
 }
